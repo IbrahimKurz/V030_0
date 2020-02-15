473df0084ec26c33b021411a95ab0e63eb2d94ca
IbrahimKurz <60773442+IbrahimKurz@users.noreply.github.com>
2020-02-15 23:55:18 +0800

    Update README.sh

--- README.sh
+++ README.sh
@@ -3,4 +3,4 @@ cat /content/V030_a/3/030r-a.* >> wrk.m2v
 
 cat /content/V030_0/A/030r-0.* >> OP.m2v
 cat /content/V030_0/A/030r-ED.* >> ED.m2v
-cat /content/V03._0/A/II-1a.* >> SND.mka
+cat /content/V030_0/A/II-1a.* >> SND.mka
